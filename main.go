eb8f57dc02fb579ff4336ac267b80ece11e82150
Matt Bargenquast <matt.bargenquast@gmail.com>
2016-02-21 21:26:34 +1100

    Migrated to codegansta's cli for command-line processing.

--- main.go
+++ main.go
@@ -5,28 +5,69 @@ import
 	"go-mod/module"
 	"log"
 	"github.com/alexcesaro/log/stdlog"
-	"flag"
 	"os"
+	"github.com/codegangsta/cli"
 )
 
 
 func main() {
-	modfilepath := flag.String("load","","Path to file to load")
+
+	app := cli.NewApp()
 	logger := stdlog.GetFromFlags()
-	flag.Parse()
-	if (*modfilepath == "") {
-		logger.Error("Usage:")
-		logger.Error("   main.go -load <modfile>")
-		os.Exit(1)
+	app.Name = "go-mod"
+	app.Usage = "Do fun things with mod files."
+	app.Author = "mrbarge"
+	app.Flags = []cli.Flag {
+		cli.StringFlag {
+			Name: "log",
+		},
+	}
+	app.Commands = []cli.Command {
+		{
+			Name: "play",
+			Usage: "Play a file.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "file",
+					Usage: "File to play",
+				},
+			},
+			Action: func(c *cli.Context) {
+				m, err := module.Load(c.String("file"))
+				if (err != nil) {
+					pt := m.(*module.ProTracker)
+					pt.Play()
+				} else {
+					log.Fatal(err)
+				}
+			},
+		},
+		{
+			Name: "info",
+			Usage: "Print info about a file.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "file",
+					Usage: "File to play",
+				},
+			},
+			Action: func(c *cli.Context) {
+				m, err := module.Load(c.String("file"))
+				pt := m.(*module.ProTracker)
+				if (err == nil) {
+					logger.Infof("%s",pt.Title())
+					logger.Infof("song length %d",pt.SongLength())
+					logger.Infof("Num patterns: %d",pt.NumPatterns())
+					for idx, instrument := range pt.Instruments() {
+						logger.Infof("Instrument %d: %s",idx,instrument.Name())
+					}
+				} else {
+					logger.Errorf("%s",err.Error())
+				}
+			},
+		},
 	}
 
-	m, err := module.Load(*modfilepath)
-	pt := m.(*module.ProTracker)
-	if (err == nil) {
-		logger.Infof("%s",pt.Title())
-		logger.Infof("song length %d",pt.SongLength())
-		logger.Infof("Num patterns: %d",pt.NumPatterns())
-	} else {
-		log.Fatal(err)
-	}
+	app.Run(os.Args)
+
 }
