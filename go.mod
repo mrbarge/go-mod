79b82aeaa9bd0400f2fafe6131f11cc1acf2057c
Matt Bargenquast <matt.bargenquast@gmail.com>
2020-03-07 15:47:57 +1000

    Revised to use go modules and urfave/cli

---
+++ go.mod
@@ -0,0 +1,9 @@
+module go-mod
+
+go 1.12
+
+require (
+	github.com/alexcesaro/log v0.0.0-20150915221235-61e686294e58
+	github.com/sirupsen/logrus v1.4.2
+	github.com/urfave/cli/v2 v2.1.1
+)
--- main.go
+++ main.go
@@ -1,122 +1,122 @@
 package main
 
-import
-(
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 	"go-mod/module"
-	"log"
-	"github.com/alexcesaro/log/stdlog"
-	"os"
-	"github.com/codegangsta/cli"
 	"io/ioutil"
+	"os"
 )
 
+func info(file string) error {
+	log.WithFields(log.Fields{
+		"file": file,
+	}).Info("Printing Info")
+
+	m, err := module.Load(file)
+	pt := m.(*module.ProTracker)
+	if (err == nil) {
+		log.WithFields(log.Fields{
+			"title": pt.Title(),
+			"song-length": pt.SongLength(),
+			"num-patterns": pt.NumPatterns(),
+		}).Info("Info")
+		for idx, instrument := range pt.Instruments() {
+			log.WithFields(log.Fields{
+				"index": idx,
+				"name": instrument.Name(),
+			}).Info("Instrument")
+		}
+		for idx, patternNum := range pt.SequenceTable() {
+			// If we've already moved past the song length in the sequence table, short circuit
+			if (idx >= int(pt.SongLength())) {
+				break
+			}
+			pattern, err := pt.GetPattern(patternNum)
+			if (err == nil) {
+				log.WithFields(log.Fields{
+					"seq-no": idx,
+					"pattern": patternNum,
+					"channel": pattern.NumChannels(),
+				}).Info()
+			}
+		}
+
+	} else {
+		return err
+	}
+
+	return nil
+}
+
+func export(infile string, index int, outfile string) error {
+	log.WithFields(log.Fields{
+		"infile": infile,
+		"outfile": outfile,
+		"index": index,
+	}).Info("Exporting sample")
+
+	m, err := module.Load(infile)
+	pt := m.(*module.ProTracker)
+	if (err != nil) {
+		return err
+	}
+	i,err := pt.GetInstrument(index)
+	if (err != nil) {
+		return err
+	}
+	outdata := i.Data()
+	err = ioutil.WriteFile(outfile, outdata,0644)
+	if (err != nil) {
+		return err
+	}
+
+	log.WithFields(log.Fields{
+		"num-bytes": len(outdata),
+		"out-file": outfile,
+	}).Info("Wrote")
+
+	return nil
+}
 
 func main() {
 
-	app := cli.NewApp()
-	logger := stdlog.GetFromFlags()
-	app.Name = "go-mod"
-	app.Usage = "Do fun things with mod files."
-	app.Author = "mrbarge"
-	app.Flags = []cli.Flag {
-		cli.StringFlag {
-			Name: "log",
-		},
-	}
-	app.Commands = []cli.Command {
-		{
-			Name: "play",
-			Usage: "Play a file.",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "file",
-					Usage: "File to play",
+	app := &cli.App {
+		Name: "go-mod",
+		Usage: "Do fun things with mod files.",
+		Commands: []*cli.Command {
+			&cli.Command {
+				Name: "info",
+				Usage: "Print info about a file.",
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "file", Usage: "File to show" },
+				},
+				Action: func(c *cli.Context) error {
+					f := c.String("file")
+					return info(f)
 				},
 			},
-			Action: func(c *cli.Context) {
-				m, err := module.Load(c.String("file"))
-				if (err != nil) {
-					pt := m.(*module.ProTracker)
-					pt.Play()
-				} else {
-					log.Fatal(err)
-				}
-			},
-		},
-		{
-			Name: "info",
-			Usage: "Print info about a file.",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "file",
-					Usage: "File to play",
+			&cli.Command{
+				Name: "extract",
+				Usage: "Extract an instrument sample.",
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "file", Usage: "File to extract from." },
+					&cli.StringFlag{Name: "out", Usage: "File to write sample to." },
+					&cli.StringFlag{Name: "idx", Usage: "Instrument index to extract (starts at 0)" },
 				},
-			},
-			Action: func(c *cli.Context) {
-				m, err := module.Load(c.String("file"))
-				pt := m.(*module.ProTracker)
-				if (err == nil) {
-					logger.Infof("%s",pt.Title())
-					logger.Infof("song length %d",pt.SongLength())
-					logger.Infof("Num patterns: %d",pt.NumPatterns())
-					for idx, instrument := range pt.Instruments() {
-						logger.Infof("Instrument %d: %s",idx,instrument.Name())
-					}
-					for idx, patternNum := range pt.SequenceTable() {
-						// If we've already moved past the song length in the sequence table, short circuit
-						if (idx >= int(pt.SongLength())) {
-							break
-						}
-						pattern, err := pt.GetPattern(patternNum)
-						if (err == nil) {
-							logger.Debugf("Sequence Number %d, Pattern: %d -> %d", idx, patternNum, pattern.NumChannels())
-						}
-					}
-				} else {
-					logger.Errorf("%s",err.Error())
-				}
-			},
-		},
-		{
-			Name: "extract",
-			Usage: "Extract an instrument sample.",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "file",
-					Usage: "File to extract from.",
+				Action: func(c *cli.Context) error {
+					f := c.String("file")
+					idx := c.Int("idx")
+					o := c.String("out")
+					return export(f, idx, o)
 				},
-				cli.StringFlag{
-					Name: "out",
-					Usage: "File to write sample to.",
-				},
-				cli.StringFlag{
-					Name: "idx",
-					Usage: "Instrument index to extract (starts at 0)",
-				},
-			},
-			Action: func(c *cli.Context) {
-				m, err := module.Load(c.String("file"))
-				pt := m.(*module.ProTracker)
-				if (err != nil) {
-					logger.Errorf("%s",err.Error())
-					os.Exit(1)
-				}
-				i,err := pt.GetInstrument(c.Int("idx"))
-				if (err != nil) {
-					logger.Errorf("%s",err.Error())
-					os.Exit(1)
-				}
-				outdata := i.Data()
-				err = ioutil.WriteFile(c.String("out"),outdata,0644)
-				if (err != nil) {
-					logger.Errorf("%s",err.Error())
-					os.Exit(1)
-				}
-				logger.Infof("Wrote %d bytes to file %s",len(outdata),c.String("out"))
 			},
 		},
 	}
 
-	app.Run(os.Args)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
